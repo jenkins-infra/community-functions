d3a1cdd97b693f897c0663e2aab54b8294b3f2a6
R. Tyler Croy <tyler@monkeypox.org>
2017-11-15 20:05:59 -0800

    Give up on the Blue Ocean REST API and simplify computing a logs_url

--- comment-logger/index.js
+++ comment-logger/index.js
@@ -2,7 +2,6 @@ module.exports = function (context, data) {
     const GithubApi = require('github');
     const http = require('follow-redirects').http;
     const https = require('follow-redirects').https;
-    const url  = require('url');
 
     const event_type = context.req.headers['x-github-event'];
     let github = new GithubApi();
@@ -46,64 +45,46 @@ module.exports = function (context, data) {
         context.done();
         return;
     }
-
     context.log('Matched on ' + target_url);
 
-    https.get(target_url, function(r) {
-        context.log('The "real" url we\'re working with is: ' + r.responseUrl);
-        /*
-        * This URL is more useful, something like:
-        *  https://ci.jenkins.io/blue/organizations/jenkins/Infra%2Fjenkins-infra/detail/PR-898/1/pipeline
-        */
-        const blueocean_url = url.parse(r.responseUrl);
+    /*
+     * The Blue Ocean URL for fetching the whole log goes through the REST
+     * API which is hugely different and inconsistent:
+     *   Redirect URL:
+     *       https://ci.jenkins.io/job/Infra/job/jenkins-infra/job/PR-898/1/display/redirect
+     *   Detail URL:
+     *       https://ci.jenkins.io/blue/organizations/jenkins/Infra%2Fjenkins-infra/detail/PR-898/1/pipeline
+     *   Pipeline Raw Output URL
+     *       https://ci.jenkins.io/blue/rest/organizations/jenkins/pipelines/Infra/pipelines/jenkins-infra/branches/PR-898/runs/1/log
+     *
+     * Compared to the classic URL
+     *       https://ci.jenkins.io/job/Infra/job/jenkins-infra/job/PR-903/1/consoleText
+     */
+    const logs_url = target_url.replace('display\/redirect', 'consoleText');
+    context.log('Grabbing logs from: ' + logs_url);
 
-        /* At this point url_parts can be joined back together to form the
-            * full and proper URL for fetching the logs:
-            *  https://ci.jenkins.io/blue/rest/organizations/jenkins/pipelines/Infra/pipelines/jenkins-infra/branches/PR-898/runs/1/log
-            */
-        let parts = blueocean_url.pathname.match(/.*PR-(\d+)\/(\d+).*/);
+    https.get(logs_url, function(l) {
+        let logData = '';
+        l.on('data', function(chunk) { logData += chunk; });
+        l.on('end', function() {
+            let lines = logData.split('\n');
 
-        if (!parts) {
-            context.done();
-            return;
-        }
-        /* Let's get stupid */
-        let pull_request = parts[1];
-        let run = parts[2];
-        context.log('Processing for pull request ' + pull_request);
+            /* Chop it down if it's too big. */
+            if (lines.length > 100) {
+                lines = lines.slice(lines.length - 100);
+            }
 
-        let path_parts = blueocean_url.pathname.split('/');
-        /* Mangle that URL! */
-        path_parts.splice(path_parts.length - 2, 2, 'runs', run, 'log');
-        const logs_url = 'https://' + blueocean_url.host + path_parts.join('/');
-        context.log('Grabbing logs from: ' + logs_url);
-
-        https.get(logs_url, function(l) {
-            let logData = '';
-            l.on('data', function(chunk) { logData += chunk; });
-            l.on('end', function() {
-                let lines = logData.split('\n');
-
-                /* Chop it down if it's too big. */
-                if (lines.length > 100) {
-                    lines = lines.slice(lines.length - 100);
-                }
-
-                github.issues.createComment({
-                        owner: data.repository.owner.login,
-                        repo: data.repository.name,
-                        number: pull_request,
-                        body: 'The context from the Jenkins Pipeline run is:\n```\n' + lines.join('\n') + '\n```'
-                });
-
-                context.done();
+            github.issues.createComment({
+                    owner: data.repository.owner.login,
+                    repo: data.repository.name,
+                    number: pull_request,
+                    body: 'The context from the Jenkins Pipeline run is:\n```\n' + lines.join('\n') + '\n```'
             });
-        }).on('error', function(e) {
-            context.log('Failed to fetch the log data for: ' + logs_url + ' due to: ' + e.message);
+
             context.done();
         });
     }).on('error', function(e) {
-        context.log('Failed to process URI: ' + target_url + ' due to: ' + e.message);
+        context.log('Failed to fetch the log data for: ' + logs_url + ' due to: ' + e.message);
         context.done();
     });
 };
