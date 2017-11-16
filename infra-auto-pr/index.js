4a1e79b153442e6acb2694bda4bd567e91b199f0
R. Tyler Croy <tyler@monkeypox.org>
2017-11-15 16:44:08 -0800

    Oops, missed an invalid field

--- infra-auto-pr/index.js
+++ infra-auto-pr/index.js
@@ -1,7 +1,7 @@
 // Please visit http://go.microsoft.com/fwlink/?LinkID=761099&clcid=0x409 for more information on settting up Github Webhooks
 module.exports = function (context, data) {
-    context.log('GitHub Webhook triggered!', data.comment.body);
     const github = require('github');
-    //context.res = { body: 'New GitHub comment: ' + data.comment.body };
+    context.log('GitHub Webhook triggered!', data.name);
+    context.log(data.context);
     context.done();
 };
