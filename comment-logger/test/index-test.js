74e05f32f2f92d18276f25dd0a6ee67569dfbc67
R. Tyler Croy <tyler@monkeypox.org>
2017-12-08 09:24:56 -0800

    Add some basic tests around the comment-logger
    Nothing seriously useful yet

--- comment-logger/test/index-test.js
+++ comment-logger/test/index-test.js
@@ -1,3 +1,37 @@
 const assert = require('assert');
+const simple = require('simple-mock');
+const fun    = require('../index.js');
 
-/* TODO: actually write tests lol */
+describe('Handling webhook events', () => {
+    var ctx = simple.mock();
+    beforeEach(() => {
+        simple.mock(ctx, 'log', (...args) => { /* console.log(args); */ });
+        simple.mock(ctx, 'done', () => {});
+    });
+    afterEach(() => { simple.restore() });
+
+    describe('Non-status events', () => {
+        it('should do nothing and return', () => {
+            simple.mock(ctx, 'req', {
+                headers: { 'x-github-event' : 'push' }
+            });
+
+            fun(ctx, {});
+
+            assert(ctx.done.called);
+        });
+    });
+
+    describe('Status events', () => {
+        beforeEach(() => {
+            simple.mock(ctx, 'req', {
+                headers: { 'x-github-event' : 'status' }
+            });
+        });
+
+        it('should do nothing on `success`', () => {
+            fun(ctx, {state: 'success'});
+            assert(ctx.done.called);
+        });
+    });
+});
