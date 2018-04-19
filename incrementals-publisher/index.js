35dbe1c55838d4003ee2f258b3aca1ede68f9678
R. Tyler Croy <tyler@monkeypox.org>
2018-04-19 13:40:46 -0700

    Add stubs for an incrementals publisher function

---
+++ incrementals-publisher/index.js
@@ -0,0 +1,4 @@
+// Please visit http://go.microsoft.com/fwlink/?LinkID=761099&clcid=0x409 for more information on settting up Github Webhooks
+module.exports = function (context, data) {
+    context.done();
+};
