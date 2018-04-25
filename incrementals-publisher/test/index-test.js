908d8d4d8dbab2babf0feee9f6b131f19afb5da8
Jesse Glick <jglick@cloudbees.com>
2018-04-25 09:37:51 -0400

    Strengthening and improving mock tests.

--- incrementals-publisher/test/index-test.js
+++ incrementals-publisher/test/index-test.js
@@ -10,29 +10,34 @@ describe('Handling incremental publisher webhook events', () => {
 
   beforeEach(() => {
       ctx.log = simple.mock();
-      simple.mock(ctx.log, 'error', (...args) => console.log(args));
+      simple.mock(ctx.log, 'info', (...args) => console.log('[INFO]', args));
+      simple.mock(ctx.log, 'error', (...args) => console.log('[ERROR]', args));
   });
   afterEach(() => { simple.restore() });
 
   describe('without parameters', () => {
-    it('should run without any parameters', () => {
-      fun(ctx, data);
-    });
-
-    it('should return a 400', () => {
+    it('should require a parameter', () => {
       fun(ctx, data);
       assert.equal(ctx.res.status, 400);
+      assert.equal(ctx.res.body, 'The incrementals-publisher invocation was missing the build_url attribute');
     });
   });
 
   describe('without a build_url matching JENKINS_HOST', () => {
-    beforeEach(() => {
-      data.body.build_url = 'https://example.com/foo/bar';
-    });
-
     it('should return a 400', () => {
+      data.body.build_url = 'https://example.com/foo/bar';
       fun(ctx, data);
       assert.equal(ctx.res.status, 400);
+      assert.equal(ctx.res.body, 'This build_url is not supported');
+    });
+  });
+
+  describe('with a weird build_url', () => {
+    it('should return a 400', () => {
+      data.body.build_url = 'https://ci.jenkins.io/junk/';
+      fun(ctx, data);
+      assert.equal(ctx.res.status, 400);
+      assert.equal(ctx.res.body, 'This build_url is malformed');
     });
   });
 });
