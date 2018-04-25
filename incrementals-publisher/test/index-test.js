76d13c1f777c78c58c11d1aaf5dbd0dab9c66876
Jesse Glick <jglick@cloudbees.com>
2018-04-25 10:25:22 -0400

    Turns out the mock tests were also not properly handling exceptions, leading to cryptic warnings about promises.

--- incrementals-publisher/test/index-test.js
+++ incrementals-publisher/test/index-test.js
@@ -7,6 +7,10 @@ describe('Handling incremental publisher webhook events', () => {
   let data = {
     body: {}
   };
+  let run = () => {
+    ctx.res = {};
+    fun(ctx, data).catch(err => console.log('Caught', err));
+  };
 
   beforeEach(() => {
       ctx.log = simple.mock();
@@ -17,7 +21,7 @@ describe('Handling incremental publisher webhook events', () => {
 
   describe('without parameters', () => {
     it('should require a parameter', () => {
-      fun(ctx, data);
+      run();
       assert.equal(ctx.res.status, 400);
       assert.equal(ctx.res.body, 'The incrementals-publisher invocation was missing the build_url attribute');
     });
@@ -26,7 +30,7 @@ describe('Handling incremental publisher webhook events', () => {
   describe('without a build_url matching JENKINS_HOST', () => {
     it('should return a 400', () => {
       data.body.build_url = 'https://example.com/foo/bar';
-      fun(ctx, data);
+      run();
       assert.equal(ctx.res.status, 400);
       assert.equal(ctx.res.body, 'This build_url is not supported');
     });
@@ -35,7 +39,7 @@ describe('Handling incremental publisher webhook events', () => {
   describe('with a weird build_url', () => {
     it('should return a 400', () => {
       data.body.build_url = 'https://ci.jenkins.io/junk/';
-      fun(ctx, data);
+      run();
       assert.equal(ctx.res.status, 400);
       assert.equal(ctx.res.body, 'This build_url is malformed');
     });
